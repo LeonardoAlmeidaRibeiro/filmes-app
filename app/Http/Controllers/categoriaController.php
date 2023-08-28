da1cb0fda7690600335f78f062e9de820879a11d
Leonardo Ribeiro <leonardoribeiro@hfa.mil.br>
2023-08-28 11:50:41 -0300

    criacao de categoria

--- app/Http/Controllers/categoriaController.php
+++ app/Http/Controllers/categoriaController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class categoriaController extends Controller
 {
-    //
+    function index()  {
+        $categorias =  Categoria::all();
+        return view('index',compact('categorias'));
+    }
 }
